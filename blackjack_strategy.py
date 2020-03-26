6d736d1df4181dd77180c40c80deb77a4201525b
guedanie <danguerrero93@gmail.com>
2020-03-25 23:28:41 -0500

    fixed structural issues

--- blackjack_strategy.py
+++ blackjack_strategy.py
@@ -13,101 +13,124 @@ def input_cards():
     return ([card_1, card_2], dealer)
 
 def black_jack_suggestion(hand, dealer_card):
-    hard_count=sum(hand) # Hard count = No Aces, or Ace == 1, not 11
-    cards = len(hand) 
-    aces_count = 1 * (1 in hand) # Counts number of Aces in card
-    soft_count = hard_count # temporary value for soft_hand
+    hard_count=sum(hand)
+    cards=len(hand)
+    aces_count= 1*(1 in hand)
+    soft_count=hard_count
     
     if cards < 2:
-        return "Need more cards"
-
-    if cards == 2 and soft_count == 21:
-        return "Blackjack"
-
-    if cards == 2:
-
-        if hand[0] == hand[1]:
-            card=hand[0]
-
-            if card == 1:
-                return "Split"
-            
-            if card==8 and dealer_card != 1:
-                return "Split"
-
-            if card in [2,3,7] and dealer_card not in [1,8,9, 10]:
-                return "Split"
-            
-            if card == 6 and dealer_card not in [1,7,8,9,10]:
-                return "Split"
-
-            if card == 4 and dealer_card in [5,6]:
-                return "Split"
+        return 'Not enough cards'
     
     if hard_count <= 11:
-        soft_count = hard_count + 10 * aces_count
-
+        soft_count= hard_count + 10 * aces_count
+        
+    if cards == 2 and soft_count== 21:
+        return 'Blackjack'
+    
+    
     if soft_count >= 19:
-        return "Stay"
-
+        return 'Stay'
+    
     if hand == [9,9] and dealer_card not in [1,7,10]:
-        return "Split"
+        return 'Split'
     
-    if hard_count >= 17:
-        return "Stay"
+    if hard_count >= 17 :
+        return 'Stay'
 
-
-    if hard_count == soft_count:
-        if hard_count == 11 and dealer_card != 1:
-            return "Double"
-        if hard_count == 10 and dealer_card not in [1,10]:
-            return "Double"
-        if hard_count == 9 and dealer_card not in [2,1,7,8,9,10]:
-            return "Double"
-        if hard_count <= 11:
-            return "Hit"
-        if hard_count == 12 and dealer_card <= 3:
-            return "Hit"
-        if dealer_card in [1,7,8,9,10]:
-            return "Hit"
-
-        return "Stay"
     
-    if hard_count != soft_count:
+    if cards>2:
+        if hard_count==soft_count:
+            if hard_count<=11:
+                return 'Hit'
 
-        if soft_count == 18:
-
-            if dealer_card not in [2,1,7,8,9,10]:
-
-                return "Double"
-
-            if dealer_card in [2, 7, 8]:
-                return "Stay"
-
-        if soft_count == 17 and dealer_card in [3, 4, 5, 6]:
-            return "Double"
-
-        if soft_count in [15,16] and dealer_card in [4,5,6]:
-            return "Double"
-        
-        if soft_count in [13,14] and dealer_card in [5,6]:
-            return "Double"
-
-        return "Hit"
-
-
-    if cards > 2:
-        if hard_count == soft_count:
-            if hard_count <= 11:
-                return "Hit"
-        
-            if hard_count == 12 and dealer_card <= 3:
-                return "Hit"
+            if hard_count==12 and dealer_card <=3:
+                return 'Hit'
 
             if dealer_card in [1,7,8,9,10]:
-                return "Hit"
+                return 'Hit'
+
+            return 'Stay'
         
-            return "Stay"
+        
+        if soft_count<=17:
+            return 'Hit'
+        
+        if soft_count==18 and dealer_card in [1,9,10]:
+            return 'Hit'
+        
+        
+        
+        return 'Stay'
+    
+            
+        
+    
+    if cards==2:
+        
+        if hand[0]==hand[1]:
+            card= hand[0]
+            
+            if card==1 or card==11:
+                return 'Split'
+            
+            if card==8 and dealer_card!=1:
+                return 'Split'
+            
+            if card in [2,3,7] and dealer_card not in [1,8,9,10]:
+                return 'Split'
+            
+            if card==6 and dealer_card not in [1,7,8,9,10]:
+                return 'Split'
+            
+            if card==4 and dealer_card in [5,6]:
+                return 'Split'
+
+            
+        if hard_count==soft_count:
+            
+            if hard_count==11 and dealer_card!=1:
+                return 'Double'
+            
+            if hard_count==10 and dealer_card not in [1,10]:
+                return 'Double'
+            
+            if hard_count==9 and dealer_card not in [1, 2,7,8,9,10]:
+                return 'Double'
+            
+            if hard_count<=11:
+                return 'Hit'
+
+            if hard_count==12 and dealer_card <=3:
+                return 'Hit'
+
+            if dealer_card in [1,7,8,9,10]:
+                return 'Hit'
+
+            return 'Stay'
+            
+        
+        if hard_count!=soft_count:
+            
+
+                
+            if soft_count==18:
+                
+                if dealer_card not in [1,2,7,8,9,10]:
+                    return 'Double'
+                
+                if dealer_card in [2,7,8]:
+                    return 'Stay'
+                
+            if soft_count==17 and dealer_card in [3,4,5,6]:
+                return 'Double'
+            
+            if soft_count in [15,16] and dealer_card in [4,5,6]:
+                return 'Double'
+            
+            if soft_count in [13,14] and dealer_card in [5,6]:
+                return 'Double'
+            
+            return 'Hit'
         
 
 player_cards, dealer_card = input_cards()
